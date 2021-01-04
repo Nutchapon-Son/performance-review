689b48dbbc983d42c9a34467f1b6f40a982ce49f
Son Nutchapon <sonnutchapon@gmail.com>
2021-01-04 17:33:03 +0700

    haha

--- shuffle/shuffle.js
+++ shuffle/shuffle.js
@@ -4,6 +4,12 @@
  * @return {number[]}
  */
 export function shuffle(cards) {
-  throw new Error('Not implemented')
-}
+  const random1 = Math.random() * 50;
+  const random2 = Math.random() * 50;
+  const temp = cards[random1];
+  cards[random1] = cards[random2];
+  cards[random2] = temp;
+  return cards;
 
+  throw new Error("Not implemented");
+}
